d7c0a9f2b13db8537439035c90230be24293e00a
i.zarovnaya <irizarov@gmail.com>
2024-02-06 22:13:23 +0200

    add new 1234_TC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello git");
         System.out.println("hello new");
+        System.out.println("hello 1234_TC");
+
     }
 }
